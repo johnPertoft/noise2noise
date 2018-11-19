52c9a374fec962583aff0abee89121b29d396f99
John Pertoft <john.pertoft@schibsted.com>
2018-11-19 21:31:43 +0100

    Add additive gaussian noise fn.

--- n2n/data/inspect.py
+++ n2n/data/inspect.py
@@ -1,9 +1,19 @@
+import numpy as np
 import tensorflow as tf
+from matplotlib import pyplot as plt
 
 from .dataset import imagenet
+from .noise import additive_gaussian_noise
 
-ds = imagenet('/efs/datasets/imagenet/train*', 64)
 
-img = ds.make_one_shot_iterator().get_next()
+ds = imagenet('/home/john/imagenet.tfrecord', 15, noise_fn=additive_gaussian_noise(0, 50))
+img1, img2, ground_truth = ds.make_one_shot_iterator().get_next()
+
 with tf.Session() as sess:
-    print(sess.run(img)[0])
+    img1, img2, ground_truth = sess.run((img1, img2, ground_truth))
+    _, axes = plt.subplots(5, 3)
+    axes = axes.flatten()
+    for i1, i2, gt, ax in zip(img1, img2, ground_truth, axes):
+        ax.imshow(np.concatenate((i1, i2, gt), axis=1))
+    plt.show()
+
---
+++ n2n/data/noise.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+# TODO: Brown noise.
+# TODO: text overlay noise.
+
+
+def additive_gaussian_noise(min_stddev, max_stddev):
+    def apply(img):
+        n = tf.shape(img)[0]
+        stddev = tf.random_uniform((n, 1, 1, 1), min_stddev, max_stddev)
+        stddev = stddev / 255.0
+        clip = lambda i: tf.clip_by_value(i, 0.0, 1.0)
+        noisy_img1 = clip(img + tf.random_normal(tf.shape(img), stddev=stddev))
+        noisy_img2 = clip(img + tf.random_normal(tf.shape(img), stddev=stddev))
+        return noisy_img1, noisy_img2
+
+    return apply

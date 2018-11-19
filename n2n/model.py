34b33d2a03651986b4ad042bacb8ad0e5db61cca
John Pertoft <john.pertoft@schibsted.com>
2018-11-19 21:45:16 +0100

    tf.estimator

--- n2n/model.py
+++ n2n/model.py
@@ -1,9 +1,37 @@
 import tensorflow as tf
 
+from .nets import red30
+
 
 def model_fn(features, labels, mode, params, config):
+    is_training = mode == tf.estimator.ModeKeys.TRAIN
 
-    # TODO: Use tf gan estimator?
+    f = tf.make_template('f', red30.model_fn, is_training=is_training)
+
+    x = features['input']
+    x_hat = f(x)
+
+    # TODO: Should the output of f be with the final activation? or output logits?
+
+    predictions = None
+    if mode in (tf.estimator.ModeKeys.PREDICT, tf.estimator.ModeKeys.EVAL):
+        pass
+
+    loss = None
+    if mode in (tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL):
+        pass
+
+    eval_metric_ops = None
+    evaluation_hooks = None
+    if mode == tf.estimator.ModeKeys.EVAL:
+        pass
+
+    train_op = None
+    training_hooks = None
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        global_step = tf.train.get_or_create_global_step()
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            pass
 
     return tf.estimator.EstimatorSpec(
         mode=mode,
---
+++ n2n/nets/red30.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def conv():
+    pass
+
+
+def dconv():
+    pass
+
+
+def model_fn(x, is_training):
+    pass
